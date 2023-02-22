e1d31d27ffe60a53ec47d068a4d1d04e12434fc1
Eli Morris <elimorris@Elis-MBP.lan>
2023-02-21 22:44:33 -0800

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Coding-Quiz-Challenge
+
+
+
+![D3F0A0A5-F1C4-4972-AC16-A932959CEB5B_1_201_a](https://user-images.githubusercontent.com/118331058/220520376-f1a89de1-0b72-496c-bb10-e5ef44550cc2.jpeg) 
+
+
+Details:
+- Creating <li> tags from question and answer arrays
+- Assign correct id to correct answer
+- Correct answer marked with correct:
+- Score is stored to DOM 
+- Sort scores from high to low
+- Every incorrect answers lowers time by 10 seconds
+- Able to restart submit score, clear score, and go back
+- Able to restart game
+
+
+About: 
+Module 4, coding quiz challenge. Timer must start when game starts. Question is then presented, with answers.
+Another question is presented after previous question is answered. When answered incorrectly, time is taken off clock(10 seconds).
+After all questions are answered, or time reaches 0, the game is over.
+Then initals/name and score is stored.
+
+
+
+
+
+
+Notes:
+Name all answers and questions to keep track of corresponding answer --> question.
+Make sure class names are spelled correctly. Make sure to commit more, and have more descriptive comments.
+Center question and button info. Cant figure out how to insert screenshot properly.
+
+
+Color scheme: 
+background: light or dark?
+button color: teal, green, red, blue?
+hover color: different color or just darker shade?
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,232 +1,255 @@
-//HTML Elements
-var timeTag = document.querySelector('#timeTag'); 
-var timePTag = document.querySelector('header').children[1];
-var submitHighScoreBtn = document.querySelector('#submitHighScoreBtn');
-var viewHighScoresBtn = document.querySelector('#viewHighScoresBtn');
-var clearHighScoresBtn = document.querySelector('#clearHighScoresBtn');
-var answerButtonLst = document.body.querySelector('ul');
-var goBackScoreBtn = document.querySelector('#goBackBtn');
-var startBtn = document.querySelector('#startBtn');
-var titleTag = document.querySelector('#title');
-//start test with 75 seconds
-//questions and answers
-var questionObj = {
-    questions: [
-        'Inside which HTML element do we put JavaScript?', //1
-        'What is the corrext JavaScript syntax to change the content of the HTML element below? <p id="demo">This is a demonstration.</p>',
-        'Where is the correct place to innsert a JavaScript?',
-        'A very useful tool used during development and debugging for printing content to the debugger is:',//4
-        'Commnoly used data types DO not include:',
-        'The condition in an if / else statement is enclosed with ______.',
-        'Arrays in JavaScript can be used to store',
-        'Strign values must be enclosed wihtin _______ when being assigned to variables.',  //8
-    ],
-    answers: [
-        ['<js>', 'correct:<script>', '<javascript>', '<scripting>'],
-        [`document.getElement("p").innerHTML = "Hello World!";`, `#demo.innerHTML = "Hello World!";`, `correct:document.getElementById("demo").innerHTML = "Hello World!";`, `document.getElementByName("p").innerHTML = "Hello World!";`],
-        [`The <head> section`, `Both the <head> section and the <body> section are correct`, `correct:The <body> section`, `The <footer> section`],
+var timerTag = document.querySelector(`#timerTag`); 
+var timerPTag  = document.querySelector(`header`).children[1]; 
+var submitHighscoreBtn = document.querySelector(`#submitHighscoreBtn`); 
+var viewHighscoresBtn = document.querySelector(`#viewHighscoresBtn`); 
+var clearHighscoreBtn = document.querySelector(`#clearHighscoreBtn`); 
+var answerButtonLst = document.body.querySelector(`ul`); 
+var goBackHighscoreBtn = document.querySelector(`#goBackBtn`);
+var startBtn = document.querySelector(`#startBtn`);
+var titleTag = document.querySelector(`#title`) 
 
-    ]
+var questionObj = { 
+    questions: [ 
+    `Inside which HTML element do we put JavaScript?`, //1
+    `What is the corrext JavaScript syntax to change the content of the HTML element below? <p id="demo">This is a demonstration.</p>`, //2
+    `Where is the correct place to innsert a JavaScript?`, //3
+    `A very useful tool used during development and debugging for printing content to the debugger is:`,//4
+    `Commnoly used data types DO not include:`, //5
+    `The condition in an if / else statement is enclosed with ______.`, //6
+    `Arrays in JavaScript can be used to store`, //7
+    `String values must be enclosed wihtin '' when being assigned to variables`,  //8
+],
+
+//Having trouble showing answers.
+//Typo with ' vs. `
+//make sure the correct: doesnt have space between answers
+answers: [
+    [`<js>`, `correct:<script>`, `<javascript>`, `<scripting>`], //1
+    [`document.getElement("p").innerHTML = "Hello World!";`, `#demo.innerHTML = "Hello World!";`, `correct:document.getElementById("demo").innerHTML = "Hello World!";`, `document.getElementByName("p").innerHTML = "Hello World!";`],//2
+    [`The <head> section`, `correct:Both the <head> section and the <body> section are correct`, `The <body> section`, `The <footer> section`],//3
+    [`JavaScript`, `terminal/bash`, `for loops`, `correct:console.log`],//4
+    [`strings`, `correct:alerts`, `numbers`, `booleans`],//5
+    [`commas`, `curly brackets`, `quotes`, `correct: parenthesis`],//6
+    [`booleans`, `correct:other arrays`, `numbers and strings`, `all of the above`], //7
+    [`commas`, `curly brackets`, `correct:quotes`, `parenthesis`]//8
+]
 }
 
+var globalTimerPreset = 75; // start time
 
-var globalTimerPreset = 75; //start time
-//variables
-var questionIndexNumber = 0;
-var timeLeft = globalTimerPreset;
-var score = 0;
-var gameEnded = true;
+
+var questionIndexNumber = 0; 
+var timeLeft = globalTimerPreset; 
+var score = 0; 
+var gameEnded = true; 
+
 
 function setUpGame() {
-    timeLeft = globalTimerPreset;
-    timeTag.textContent = globalTimerPreset;
+    timeLeft = globalTimerPreset; 
+    timerTag.textContent = globalTimerPreset; 
+    
+    document.querySelector(`#display-highscore-div`).style.display = `none`; 
 
-    document.querySelector('#display-highscore-div').getElementsByClassName.display = 'none';
+    
+    titleTag.textContent = `Coding Quiz Challenge`; 
 
-    titleTag.textContent = 'Coding Quiz Challenge';
-
-    titleTag.styleDisplay = 'block';
-    document.querySelector('#instructions').style.display = 'block';
-    viewHighScoresBtn.style.display = 'block';
-    startBtn.style.display = 'block';
+    
+    titleTag.style.display = `block`; 
+    document.querySelector(`#instructions`).style.display = `block`; 
+    viewHighscoresBtn.style.display = `block`; 
+    startBtn.style.display = `block`; 
 
     return;
 }
+
 
 function startGame() {
-    gameEnded = 'false';
-    questionIndexNumber = 0;
+    gameEnded = false; 
+    questionIndexNumber = 0; 
 
-    viewHighScoresBtn.style.display = 'none'
-    startBtn.style.display = 'none';
-    document.querySelector('#instructions').style.display = 'none';
-    timePTag.style.display = 'block';
+    
+    viewHighscoresBtn.style.display = `none` 
+    startBtn.style.display = `none`; 
+    document.querySelector(`#instructions`).style.display = `none`; 
+    timerPTag.style.display = `block`; 
 
-    showQuestions(questionIndexNumber);
-    startTimer();
+    showQuestions(questionIndexNumber); 
+    startTimer(); 
 
     return;
 }
+
 
 function startTimer() {
     var timerInterval = setInterval(function() {
-        if(gameEnded === true) {
-            clearInterval(timerInterval);
+        if(gameEnded === true) { 
+            clearInterval(timerInterval); 
             return;
         }
-        if(timeLeft < 1) {
-            clearInterval(timerInterval);
-            return;
-    }
+        if(timeLeft < 1) { 
+            clearInterval(timerInterval); 
+            endGame(); 
+        }
 
-    timeTag.textContent = timeLeft;
-    timeLeft--;
-}, 1000);
+        timerTag.textContent = timeLeft; 
+        timeLeft--; 
+    }, 1000); 
 
-return;
+    return;
 }
 
 function showQuestions(currentQuestionIndex) {
-    titleTag.textContent = questionObj.questions[currentQuestionIndex];
-    createAnswerElements(currentQuestionIndex);
+    titleTag.textContent = questionObj.questions[currentQuestionIndex]; 
+    createAnswerElements(currentQuestionIndex); 
 
     return;
 }
+
 
 function createAnswerElements(currentQuestionIndex) {
-    answerButtonLst.innerHTML = '';
+    answerButtonLst.innerHTML = ''; 
 
-    for (let answerIndex = 0; answerIndex < questionObj.answers[currentQuestionIndex].length; answerIndex++) {
-        var currentAnswerListItem = document.createElement('li');
-        var tempStr = questionObj.answers[currentQuestionIndex];
+    for (let answerIndex = 0; answerIndex < questionObj.answers[currentQuestionIndex].length; answerIndex++) { //loop over every answer (for current question) and create a list item on the page based on that content
+        var currentAnswerListItem = document.createElement(`li`); 
+        var tempStr = questionObj.answers[currentQuestionIndex][answerIndex]; //temp incase the string contains the `correct` answer tag and needs to be pulled out.
 
         if (questionObj.answers[currentQuestionIndex][answerIndex].includes(`correct:`)){
-            tempStr = questionObj.answsers[currentQuestionIndex][answerIndex].string(8, questionObj.answers[currentQuestionIndex][answerIndex].length);
-            currentAnswerListItem.id = 'correct';
+            tempStr = questionObj.answers[currentQuestionIndex][answerIndex].substring(8, questionObj.answers[currentQuestionIndex][answerIndex].length); //yoink out the string part that doesnt contain `correct:`
+            currentAnswerListItem.id = `correct`; 
         }
 
-        return;
+        currentAnswerListItem.textContent = tempStr; 
+        answerButtonLst.appendChild(currentAnswerListItem); 
     }
+
+    return;
 }
+
 
 function nextQuestion() {
-    questionIndexNumber++;
-    if(questionIndexNumber >= questionObj.questionlength){
-        endGame();
-    } else {
-        showQuestions(questionIndexNumber);
-    }
+    questionIndexNumber++; 
+    if (questionIndexNumber >= questionObj.questions.length){ 
+        endGame(); 
+    } else { 
+        showQuestions(questionIndexNumber); 
+    } 
 
     return;
 }
 
-function endGame() {
-    gameEnded = true;
-    score = timeLeft;
+function endGame() { 
+    gameEnded = true; 
+    score = timeLeft; 
 
-    timePTag.style.display = 'none';
-    titleTag.style.display = 'none';
-    answerButtonLst.innerHTML = '';
+    timerPTag.style.display = `none`; 
+    titleTag.style.display = `none`; 
+    answerButtonLst.innerHTML = ''; 
 
-    document.querySelector('#scoreSpan').textContent = score;
-    document.querySelector('#submit-highscore-div').style.display = 'block';
+    document.querySelector(`#scoreSpan`).textContent = score;
+    document.querySelector(`#submit-highscore-div`).style.display = `block`; 
 
     return;
 }
+
 
 function checkAnswer(event) {
-    if (event.target != answerButtonLst){
+    if (event.target != answerButtonLst){ 
         if (!(event.target.id.includes('correct'))){
-    }
+            timeLeft -= 10; 
+        }
 
-    nextQuestion();
+        nextQuestion();
     }
 
     return;
 }
 
 function storeScoreAndName() {
-    var highscoreTextbox = document.querySelector('input');
-    var tempArrayOfObjects = [];
+    var highscoreTextbox = document.querySelector(`input`); 
+    var tempArrayOfObjects = []; 
 
-    if(highscoreTextbox.value != `` || highscoreTextbox.value != null) {
+    if (highscoreTextbox.value != `` || highscoreTextbox.value != null) { 
         var tempObject = {
-            names: highscoreTextbox.value,
-            scores: score,
+            names: highscoreTextbox.value, 
+            scores: score, 
         }
 
-        if(window.localStorage.getItem('highscores') == null) {
-            tempArrayOfObjects.push(tempObject);
-            window.localStorage.setItem('highscores', JSON.stringify(tempArrayOfObjects));
+        if(window.localStorage.getItem(`highscores`) == null) { 
+            tempArrayOfObjects.push(tempObject); 
+            window.localStorage.setItem(`highscores`, JSON.stringify(tempArrayOfObjects)); 
 
-        
         } else {
-            tempArrayOfObjects = JSON.parse(wind.localStorage.getItem('highscores'));
+            tempArrayOfObjects = JSON.parse(window.localStorage.getItem(`highscores`)); 
 
-            for (let index = 0; index <= tempArrayOfObjects.length; index++) {
-                if (index = tempArrayOfObjects.length) {
-                    tempArrayOfObjects.push(tempObject);
-                    break;
+            for (let index = 0; index <= tempArrayOfObjects.length; index++) { 
+                if (index == tempArrayOfObjects.length) { 
+                    tempArrayOfObjects.push(tempObject) 
+                    break; 
+                } else if (tempArrayOfObjects[index].scores < score) { 
+                    tempArrayOfObjects.splice(index, 0, tempObject); 
+                    break; 
                 }
             }
-            window.localStorage.setItem('highscores', JSON.stringify(tempArrayOfObjects))
-        } 
-        document.querySelector('input').value = '';
+            window.localStorage.setItem(`highscores`, JSON.stringify(tempArrayOfObjects)) 
+        }
+        document.querySelector(`input`).value = ``; 
         score = 0;
 
-        showHighScores();
+        showHighscores(); 
     }
 
     return;
 }
 
-function showHighScores() {
-    titleTag.style.display = 'none';
-    startBtn.style.display = 'none';
-    document.querySelector('header').children[0].style.display = 'none';
-    document.querySelector('#instructions').style.display = 'none';
-    document.querySelector('#submit-highscore-div').style.display = 'none';
+function showHighscores() {
+    titleTag.style.display = `none`;
+    startBtn.style.display = `none`; 
+    document.querySelector(`header`).children[0].style.display = `none`; 
+    document.querySelector(`#instructions`).style.display = `none`; 
+    document.querySelector(`#submit-highscore-div`).style.display = `none`; 
 
-    document.querySelector('#display-highscore-div').style.display = 'block';
-     
-    tempOrderList = document.querySelector('ol');
-    tempOrderList.innerHTML = ''
+    document.querySelector(`#display-highscore-div`).style.display = `block`;
 
-    tempArrayOfObjects = JSON.parse(window.localStorage.getItem('highscores'));
-    if (tempArrayOfObjects != null) {
+    tempOrderedList = document.querySelector(`ol`); 
+    tempOrderedList.innerHTML = `` 
+
+    tempArrayOfObjects = JSON.parse(window.localStorage.getItem(`highscores`));
+    if (tempArrayOfObjects != null) { 
         for (let index = 0; index < tempArrayOfObjects.length; index++) {
-            var newLi = document.createElement('li')
-            newLi.textContent = tempArrayOfObjects[index].names + '-' + tempArrayOfObjects[index].scores;
-            tempOrderList.appendChild(newLi);
+            var newLi = document.createElement(`li`) 
+            newLi.textContent = tempArrayOfObjects[index].names + ` - ` + tempArrayOfObjects[index].scores; 
+            tempOrderedList.appendChild(newLi); 
         }
-    } else {
-        var newLi = document.createElement('p')
-        newLi.textContent = 'No Highscores'
-        tempOrderList.appendChild(newLi);
+
+    } else { 
+        var newLi = document.createElement(`p`) 
+        newLi.textContent = `No Highscores` 
+        tempOrderedList.appendChild(newLi); 
     }
 
     return;
 }
 
-function clearHighScores() {
-    document.querySelector('ol').innerHTML = '';
-    window.localStorage.clear();
+function clearHighscores() {
+    document.querySelector(`ol`).innerHTML = ``; 
+    window.localStorage.clear(); 
 
-    setUpGame();
+    setUpGame(); 
 
     return;
 }
 
 function init() {
-    startBtn.addEventListener('click', startGame);
-    answerButtonLst.addEventListener('click', checkAnswer);
-    viewHighScoresBtn.addEventListener('click', showHighScores);
-    clearHighScoresBtn.addEventListener('click', clearHighScores);
-    goBackScoreBtn.addEventListener('click', setUpGame);
+    startBtn.addEventListener(`click`, startGame); 
+    answerButtonLst.addEventListener(`click`, checkAnswer); 
+    viewHighscoresBtn.addEventListener(`click`, showHighscores); 
+    submitHighscoreBtn.addEventListener(`click`, storeScoreAndName); 
+    clearHighscoreBtn.addEventListener(`click`, clearHighscores); 
+    goBackHighscoreBtn.addEventListener(`click`, setUpGame); 
 
-    setUpGame();
+    setUpGame(); 
 
     return;
 }
 
-init();
+init(); 
